57ff585eebbeff3407748d0752c3e7574bd646d2
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2023-12-31 18:05:30 +0100

    Add keys page

---
+++ app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CheckCheck, Copy } from "lucide-react";
+import { useState } from "react";
+
+interface CopyButtonProps {
+  value?: string;
+}
+
+export const CopyButton = ({ value }: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = () => {
+    if (!value) return;
+
+    setIsCopied(true);
+    navigator.clipboard.writeText(value);
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 1000)
+  }
+
+  const Icon = isCopied ? CheckCheck : Copy;
+
+  return <Button
+    onClick={onCopy}
+    disabled={!value || isCopied}
+    variant="ghost"
+    size="sm"
+  >
+    <Icon className="size-4" />
+  </Button>;
+};
