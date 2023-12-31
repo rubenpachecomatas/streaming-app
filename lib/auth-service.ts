84a8207502f834e7819750d260d2f69d90172345
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2023-12-31 13:05:18 +0100

    Add dashboard page

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -13,11 +13,35 @@ export const getSelf = async () => {
     where: {
       externalUserId: self.id,
     },
-  })
+  });
 
   if (!user) {
     throw new Error("Not found");
   }
 
   return user;
-}
+};
+
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthourized");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (self.username !== user.username) {
+    throw new Error("Unauthorized");
+  }
+
+  return user;
+};
