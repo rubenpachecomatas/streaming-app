54688a61cc1b1548d78ec55eda63e00f16ad1662
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-06 15:03:19 +0100

    Add Stream Header component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,6 +8,7 @@ import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPlayerProps {
   isFollowing: boolean;
@@ -24,7 +25,7 @@ export const StreamPlayer = ({
   const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
-    return <StreamPlayerSkeleton />
+    return <StreamPlayerSkeleton />;
   }
 
   return (
@@ -44,6 +45,14 @@ export const StreamPlayer = ({
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -66,7 +75,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/* TODO: Header Skeleton */}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg:background">
         <ChatSkeleton />
---
+++ components/verified-mark.tsx
@@ -0,0 +1,9 @@
+import { Check } from "lucide-react";
+
+export const VerifiedMark = () => {
+  return (
+    <div className="p-0.5 flex items-center justify-center size-4 rounded-full bg-blue-600">
+      <Check className="size-[10px] text-primary stroke-[4px]" />
+    </div>
+  )
+}
