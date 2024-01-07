39b11161138421dedcc5a6d4c087611e223280f1
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-07 20:04:06 +0100

    Add Info modal

---
+++ components/stream-player/info-modal.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { ElementRef, useRef, useState, useTransition } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { updateStream } from "@/actions/stream";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+interface InfoModalProps {
+  initialName: string;
+  initialThumbnailUrl: string | null;
+}
+
+export const InfoModal = ({
+  initialName,
+  initialThumbnailUrl,
+}: InfoModalProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    startTransition(() => {
+      updateStream({ name: name })
+        .then(() => {
+          toast.success("Stream updated");
+          closeRef.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" size="sm" className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit stream info</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-14">
+          <div className="space-y-2">
+            <Label>Name</Label>
+            <Input
+              disabled={isPending}
+              placeholder="Stream name"
+              onChange={onChange}
+              value={name}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label>
+              Thumbnail
+            </Label>
+            <div className="rounded-xl border outline-dashed outline-muted">
+              <UploadDropzone
+                endpoint="imageUploader"
+                appearance={{
+                  label: {
+                    color: "#FFFFFF"
+                  },
+                  allowedContent: {
+                    color: "#FFFFFF"
+                  }
+                }}
+                onClientUploadComplete={(res) => {
+                  setThumbnailUrl(res?.[0]?.url);
+                  router.refresh();
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <DialogClose ref={closeRef} asChild>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button variant="primary" type="submit" disabled={isPending}>
+              Save
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,11 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhooks(.*)"
-  ]
+    "/api/webhooks(.*)",
+    "/api/uploadthing",
+    "/:username",
+    "/search",
+  ],
 });
 
 export const config = {
